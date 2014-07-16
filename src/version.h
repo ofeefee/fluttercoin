0b6c070d2c8d295f8fc25c731facebd7c7718453
ofeefee <ofeefee@gmail.com>
2014-07-15 19:09:21 -0500

    version update 4.5.9

--- src/version.h
+++ src/version.h
@@ -52,6 +52,6 @@ static const int MEMPOOL_GD_VERSION = 70000;
 #define DISPLAY_VERSION_MAJOR       0
 #define DISPLAY_VERSION_MINOR       4
 #define DISPLAY_VERSION_REVISION    5
-#define DISPLAY_VERSION_BUILD       8 
+#define DISPLAY_VERSION_BUILD       9 
 
 #endif

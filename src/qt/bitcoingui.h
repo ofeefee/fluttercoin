a20a05473ebb621004a259e617d989a4ce06937d
ofeefee <ofeefee@gmail.com>
2014-08-18 16:29:27 -0500

    gui unlock for staking changes

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -171,6 +171,8 @@ private slots:
     void changePassphrase();
     /** Ask for passphrase to unlock wallet temporarily */
     void unlockWallet();
+    /** Ask for passphrase to unlock wallet for staking */
+    void unlockWalletStaking();
 
     /** Show window if hidden, unminimize when minimized, rise when obscured or show if hidden and fToggleHidden is true */
     void showNormalIfMinimized(bool fToggleHidden = false);

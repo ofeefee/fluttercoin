58ed519afb7025fe8b2077c07380ba0b2fc1c32d
ofeefee <ofeefee@gmail.com>
2015-02-10 22:24:00 -0600

    formatting changes

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1953,8 +1953,8 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
         CTxDB txdb("r");
         if (!txNew.GetCoinAge(txdb, nCoinAge))
             return error("CreateCoinStake : failed to calculate coin age");
+
         nReward = GetProofOfStakeReward(nCoinAge, nBits, txNew.nTime);
-        
         // Refuse to create mint that has zero or negative reward
         if(nReward <= 0)
             return false;

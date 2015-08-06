8d62553bfae7eb5b7f9a1d206f5a141f64a0c86b
ofeefee <ofeefee@gmail.com>
2015-08-06 02:52:20 -0500

    version update protocol update

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70507;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70013;
+static const int PROTOCOL_VERSION = 70014;
 
 // earlier versions not supported and are disconnected
 static const int MIN_PROTO_VERSION = 70010;
@@ -51,8 +51,8 @@ static const int MEMPOOL_GD_VERSION = 70000;
 
 #define DISPLAY_VERSION_MAJOR       0
 #define DISPLAY_VERSION_MINOR       7
-#define DISPLAY_VERSION_REVISION    5
-#define DISPLAY_VERSION_BUILD       17
+#define DISPLAY_VERSION_REVISION    6
+#define DISPLAY_VERSION_BUILD       0
 
 static const int DISPLAY_VERSION =
                            1000000 * DISPLAY_VERSION_MAJOR
